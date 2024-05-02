0268a36c402309e9614edba95e57f9095193e913
Aleksandr-flip <81865078+Aleksandr-flip@users.noreply.github.com>
2024-05-02 14:03:37 +0200

    Add files via upload

---
+++ js/siteR.js
@@ -0,0 +1,82 @@
+﻿document.addEventListener('keydown', function (e) {
+    console.log(e.key);
+
+    if (e.key === 'Enter') {
+        alertNum();
+    }
+
+    else if (e.keyCode === 32) {
+        let findElement1 = document.getElementById('inp1');
+        findElement1.value = "";
+        let findElement2 = document.getElementById('inp2');
+        findElement2.value = "";
+
+    }
+
+});
+
+
+function alertNum() {
+        let a = parseFloat(document.getElementById('inp1').value);
+        let b = parseFloat(document.getElementById('inp2').value);
+        const out = document.querySelector('.calc-screen p');
+        
+
+    function clearAll() {
+        out.textContent = '';
+        
+        }
+
+        document.querySelector('.reset').onclick = clearAll;
+
+        document.addEventListener('keydown', function (e) {
+            console.log(e.key);
+
+            if (e.keyCode === 32) {
+                clearAll();
+                }
+        });
+
+        if (a < 0 || b < 0 || a == 0 || b == 0) {
+            let p;
+            p = document.getElementById('out');
+            out.textContent = 'Во введенных значениях допущены ошибки';
+        }
+
+        else if (a > b) {
+            let p;
+            p = document.getElementById('out');
+            out.textContent = 'Эквивалентное сопротивление не может быть больше сопротивления резистора';
+
+        }
+
+        else if (a == b) {
+            let p;
+            p = document.getElementById('out');
+            out.textContent = 'Подключение второго резистора не требуется';
+
+        }
+
+        else {
+
+            let num = 1 / ((1 / a) - (1 / b));
+
+            num = (num.toFixed(4));
+
+            if (num > 0) {
+                let p;
+                p = document.getElementById('out');
+                out.textContent = 'Расчет выполнен Rд = ' + num + ' kOm';
+            }
+
+            else {
+                let p;
+                p = document.getElementById('out');
+                out.textContent = 'Нет расчетных данных или в них допущены ошибки';
+
+            }
+        }
+}
+
+
+
